f0c4142df1a5d5a2ab79810c586ec6b83f92fca8
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-07-27 17:31:20 +0300

    DataTable was included in the project

---
+++ StockManagement.Web/wwwroot/js/dataTables-config.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $('#myTable').DataTable({
+        language: {
+            info: "_TOTAL_ records from _START_ - _END_ are shown.",
+            infoEmpty: "No records to show.",
+            loadingRecords: "Loading records...",
+            lengthMenu: "Show _MENU_ records per page",
+            zeroRecords: "No matching records found",
+            search: "Search:",
+            infoFiltered: "(filtered from _MAX_ total records)",
+            buttons: {
+                copyTitle: "Copied to clipboard.",
+                copySuccess: {
+                    1: "1 row copied to clipboard",
+                    _: "%d rows copied to clipboard"
+                },
+                copy: "Copy",
+                print: "Print",
+            },
+            paginate: {
+                first: "First",
+                previous: "Previous",
+                next: "Next",
+                last: "Last"
+            },
+        }
+    });
+});
